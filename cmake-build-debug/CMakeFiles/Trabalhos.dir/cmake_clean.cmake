8d51a4c2e10311e6a30c13f0fa6ec815bfcf00ac
Matheus <matheus_03@hotmail.com>
2018-08-16 22:01:31 -0300

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Trabalhos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabalhos.dir/main.cpp.o"
+  "Trabalhos.pdb"
+  "Trabalhos"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trabalhos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Trabalhos.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Trabalhos.
+# This may be replaced when dependencies are built.
