b486cb3fbb4b4a98daffdd2b5474902d2747614c
Matheus <matheus_03@hotmail.com>
2018-08-20 22:00:24 -0300

    Mouse callback Selection of points

--- main.cpp
+++ main.cpp
@@ -6,54 +6,105 @@ using namespace std;
 using namespace cv;
 
 char *window = (char*) "Image";
-FILE* file;
 bool leftButtonDown = false;
+int k;
+Point2f *pt;
 
+void menu();
+void L1_norm();
+void sel_methods();
 static void points(int event, int x, int y, int, void*);
+
 
 int main(int argc, char *argv[]){
 
+    /// Read the image
     Mat img = imread(argv[1]);
-    file = fopen(argv[2], "w");
 
     if (img.empty()){
         cout << "Error loading the image" << endl;
         return -1;
     }
 
-    namedWindow(window, 1);
+    /// Create first image window
+    namedWindow(window, WINDOW_NORMAL);
     imshow(window, img);
 
-    cout << "Select the points to be analyzed" << endl;
+    while(pt == NULL);
+    cout << pt->x <<",," << pt->y << endl;
+    waitKey(0);
 
+    /// Set mouse callback
     setMouseCallback(window, points, &img);
 
-    for(;;){
-        int c = waitKey(0);
+    /// Display info
+    menu();
 
-        if (c == 13){
-            cout << "Exit" << endl;
+    sel_methods();
+
+    waitKey(0);
+
+    return 0;
+}
+
+void menu(){
+    cout << "Method: " << endl;
+    cout << "1 - L1-norm (cube)" << endl;
+    cout << "2 - L2-norm (sphere)" << endl;
+    cout << "3 - Mahalanobis" << endl;
+    cout << "4 - k nearest neighbor" << endl;
+    cout << "Enter - Reset" << endl;
+    cout << "Esc - Exit" << endl;
+}
+
+void L1_norm(){
+    int th;
+
+    cout << "Select one point" << endl;
+    cout << "Threshold: ";
+    cin >> th;
+
+    while(pt == NULL);
+
+
+
+}
+
+void sel_methods(){
+
+    for(;;){
+        k = waitKey(0);
+
+        switch(k){
+            case 49:        /// Key = 1
+                L1_norm();
+                break;
+            default:
+                break;
+        }
+
+        if(k == 23){        ///Key = Esc
             destroyAllWindows();
             break;
         }
     }
-    waitKey(0);
-
-    fclose(file);
-
-    return 0;
 }
+
 
 static void points(int event, int x, int y, int, void* img_){
     Vec3b color(255, 255, 255);
     Mat &img = *(Mat*) img_;
 
+    if(event == CV_EVENT_LBUTTONDOWN && (k == 49 || k == 50)){
+        pt->x = x;
+        pt->y = y;
+        return;
+    }
+
     if (event == CV_EVENT_LBUTTONDOWN)
         leftButtonDown = true;
-    else if(event == CV_EVENT_MOUSEMOVE && leftButtonDown){
+    else if(event == CV_EVENT_MOUSEMOVE && leftButtonDown)
         img.at<Vec3b>(Point(x, y)) = color;
-        fprintf(file, "%d %d\n", x, y);
-    }
     else if(event == CV_EVENT_LBUTTONUP)
         leftButtonDown = false;
 
